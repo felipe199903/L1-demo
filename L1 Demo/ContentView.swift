8c6000b863bcdc1d1eef0f4fedef63973a9e96ec
Felipe Schorles <felipeschorles@outlook.com>
2022-12-10 12:38:08 -0300

    feature/initial

---
+++ L1 Demo/ContentView.swift
@@ -0,0 +1,35 @@
+//
+//  ContentView.swift
+//  L1 Demo
+//
+//  Created by Felipe Schorles on 09/12/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        ZStack {
+            Color(.black)
+                .ignoresSafeArea()
+            
+            VStack {
+                Image("sea")
+                    .resizable()
+                    .cornerRadius(15)
+                    .aspectRatio(contentMode: .fit)
+                    .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+                Text("Beach from the top")
+                    .font(/*@START_MENU_TOKEN@*/.largeTitle/*@END_MENU_TOKEN@*/)
+                    .fontWeight(.semibold)
+                    .foregroundColor(Color.white)
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ L1 Demo/L1_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  L1_DemoApp.swift
+//  L1 Demo
+//
+//  Created by Felipe Schorles on 09/12/22.
+//
+
+import SwiftUI
+
+@main
+struct L1_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
